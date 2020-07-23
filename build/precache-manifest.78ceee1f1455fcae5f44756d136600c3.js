bbd07b1b1347601912afc2e280080cbac60f809c
Russell <rkim846@gmail.com>
2020-07-23 12:08:07 -0400

    update

---
+++ build/precache-manifest.78ceee1f1455fcae5f44756d136600c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9823d16bc8b93f5a34ccdaebc5294b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dab3ccf8cb7412970826",
+    "url": "/static/css/main.5bc859a5.chunk.css"
+  },
+  {
+    "revision": "3ee90c544146fa4e146f",
+    "url": "/static/js/2.6d7a2951.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d7a2951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab3ccf8cb7412970826",
+    "url": "/static/js/main.4667b8ab.chunk.js"
+  },
+  {
+    "revision": "7af1f8eb3b379fc33c61",
+    "url": "/static/js/runtime-main.c5c57eb2.js"
+  }
+]);
